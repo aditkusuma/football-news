6d48c42065c7cf8387aa7a28ad647ac647a28e29
adit <radityakusuma2017@gmail.com>
2025-08-27 19:52:38 +0700

    INSYAALLAH1

--- football_news/urls.py
+++ football_news/urls.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.http import HttpResponse
-
-def health(_): return HttpResponse("ok")
+from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
