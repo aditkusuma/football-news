b98459376d587a67235472f4da3934e61e08f0e0
adit <radityakusuma2017@gmail.com>
2025-09-11 08:27:56 +0700

    ganti schema

--- football_news/settings.py
+++ football_news/settings.py
@@ -92,7 +92,7 @@ if PRODUCTION:
             'HOST': os.getenv('DB_HOST'),
             'PORT': os.getenv('DB_PORT'),
             'OPTIONS': {
-                'options': f"-c search_path={os.getenv('SCHEMA', 'public')}"
+                'options': f"-c search_path={os.getenv('SCHEMA', 'tugas_kelompok')}"
             }
         }
     }
